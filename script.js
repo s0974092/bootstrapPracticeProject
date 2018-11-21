51298798d98d9e80b16562de41ad4e8585db174b
jason <jason@luciditv.com>
2018-11-21 11:00:37 +0800

    11/21 第五次bootstrap課程進度

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(() => {
-  
+
+  $('.close-alert').click(() => {
+      // $('.alert').alert('close')
+      $('#test').alert('close')
+  })
+
+  $('.alert').on('close.bs.alert', () => {
+      console.log('close alert')
+  })
+
+  $('.alert').on('closed.bs.alert', () => {
+      console.log('closed alert')
+  })
 })
