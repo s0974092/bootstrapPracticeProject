fbd4f61542f324fe7ff6f8b5512e42c079385558
jason <jason@luciditv.com>
2018-12-18 14:20:07 +0800

    12/12 第六次bootstrap課程進度
    第六次bootstrap課程進度

--- script.js
+++ script.js
@@ -12,4 +12,24 @@ $(document).ready(() => {
   $('.alert').on('closed.bs.alert', () => {
       console.log('closed alert')
   })
+
+  $('.carousel').carousel({
+    // interval: 1000
+  })
+
+  $('.carousel').on('slide.bs.carousel', (event) => {
+    console.log('slide:', `
+      方向： ${ event.direction }
+      從： ${ event.from }
+      到： ${ event.to }
+    `)
+  })
+
+  $('.carousel').on('slid.bs.carousel', (event) => {
+    console.log('slid:', `
+      方向： ${ event.direction }
+      從： ${ event.from }
+      到： ${ event.to }
+    `)
+  })
 })
