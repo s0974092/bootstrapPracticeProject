97f9f88c097019f806a7b6fd080e484c33b3c32e
jason <jason@luciditv.com>
2019-01-09 10:31:29 +0800

    1/9 第七次bootstrap課程進度

--- script.js
+++ script.js
@@ -32,4 +32,17 @@ $(document).ready(() => {
       到： ${ event.to }
     `)
   })
+
+  $('#dropdown-demo').on('show.bs.dropdown', () => {
+    console.log('show')
+  })
+  $('#dropdown-demo').on('shown.bs.dropdown', () => {
+    console.log('shown')
+  })
+  $('#dropdown-demo').on('hide.bs.dropdown', () => {
+    console.log('hide')
+  })
+  $('#dropdown-demo').on('hidden.bs.dropdown', () => {
+    console.log('hidden')
+  })
 })
